e77f5a2ed2a1ca8e7c460443a289aacbdfe41603
Madhavi Jayasinghe <madhavi.jayasinghe@ifsworld.com>
2022-10-28 08:27:35 +0530

    PDF suggestion

--- src/components/materials/Materials.jsx
+++ src/components/materials/Materials.jsx
@@ -5,15 +5,13 @@ import { useEffect, useState } from "react";
 import { styles } from "../helpers";
 
 
-const Materials = () =>{
+const Materials = () => {
     const postBody = {
         keyWord: localStorage.getItem('cat')
 
     };
 
-      
-
-    const[items, setItems] = useState([]);
+    const [items, setItems] = useState([]);
 
     useEffect(() => {
         console.log('items', items)
@@ -29,22 +27,39 @@ const Materials = () =>{
 
     };
     useEffect(() => {
-        axios.post('http://127.0.0.1:8000/getResource', {keyWord : localStorage.getItem('category')}).then((data) => setItems(data.data))
-        //fetch('http://127.0.0.1:8000/getResource', requestMetadata).then((data) => console.log('dat', data));
-    },[]);
-    return(
-      
-     <center><br/><br/> <div>
-        <h3>You can study below resources to improve your skils</h3><br/><br/><br/>
-            {items.length > 0 && items.map((element, i ) => (
+        var category = localStorage.getItem('category')
+        var findingWord = 'oop'
+        var myPattern = new RegExp('(\\w*' + findingWord + '\\w*)', 'gi');
+
+        var matches = category.match(myPattern);
+
+        if (matches === null) {
+            console.log("No results"); // Any message or empty
+            return;
+        }
+        else {
+            axios.post('http://127.0.0.1:8000/getResource', { keyWord: 'oop ' }).then((data) => setItems(old => [...old, ...data.data]))
+            axios.post('http://127.0.0.1:8000/getResource', { keyWord: 'Object Oriented Programming' }).then((data) => setItems(old => [...old, ...data.data]))
+            axios.post('http://127.0.0.1:8000/getResource', { keyWord: 'Object-Oriented Programming' }).then((data) => setItems(old => [...old, ...data.data]))
+        }
+
+        // axios.post('http://127.0.0.1:8000/getResource', {keyWord : localStorage.getItem('category')}).then((data) => setItems(data.data))
+    }, []);
+    return (
+
+        <center><br /><br /> <div>
+            <h3>You can study below resources to improve your skils</h3><br /><br /><br />
+            {items.length > 0 && items.map((element, i) => (
                 element.map((ele, i) => (
-                    <div key={i}>{ele}</div>
+                    <a href={require('../../../src/pdf/' + ele.substring(0, ele.indexOf(' ')))} target="_blank">
+                        <div key={i}>{ele}</div>
+                    </a>
                 ))
-                
-            ) )}
-            <br/><br/><br/>
+
+            ))}
+            <br /><br /><br />
         </div></center>
-        
+
     );
 
 }
